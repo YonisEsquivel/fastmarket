b92d4d0669b0a319b460fb58b0cb64054dc06ef9
Yonis Esquivel <info.contactoprofesional@gmail.com>
2022-11-11 11:07:54 -0500

    Estructura base de la app

--- app/src/main/java/com/example/fastmarket/view/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/fastmarket/view/ui/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.fastmarket
+package com.example.fastmarket.view.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.fastmarket.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
