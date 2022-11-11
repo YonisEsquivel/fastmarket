4c74d2cf37d9c93e8857e9ae34f53d8d1e86f87d
Yonis Esquivel <info.contactoprofesional@gmail.com>
2022-11-11 15:16:30 -0500

    Vista registro

--- app/src/main/java/com/example/fastmarket/view/ui/activities/LoginActivity.kt
+++ app/src/main/java/com/example/fastmarket/view/ui/activities/LoginActivity.kt
@@ -1,12 +1,27 @@
 package com.example.fastmarket.view.ui.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.fastmarket.R
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var registrobutton:Button
+    lateinit var iniciobutton:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        iniciobutton=findViewById(R.id.btningresarLogin)
+        registrobutton=findViewById(R.id.btncrearLogin)
+
+        registrobutton.setOnClickListener {
+            startActivity(Intent(this, RegistroActivity::class.java))
+        }
+        iniciobutton.setOnClickListener {
+            startActivity(Intent())
+        }
     }
 }
