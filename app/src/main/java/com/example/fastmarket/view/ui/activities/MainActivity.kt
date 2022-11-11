1ae4c5468985709bb9ba1e576f118cd49d6a7feb
Yonis Esquivel <info.contactoprofesional@gmail.com>
2022-11-11 12:45:51 -0500

    Animación de inicio

--- app/src/main/java/com/example/fastmarket/view/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/fastmarket/view/ui/activities/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.example.fastmarket.view.ui.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.example.fastmarket.R
+import com.example.fastmarket.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
+    lateinit var handler: Handler
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.animacion.setAnimation(R.raw.animacion)
+        binding.animacion.playAnimation()
+
+        handler = Handler(Looper.myLooper()!!)
+
+        handler.postDelayed({
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 4000)
     }
 }
