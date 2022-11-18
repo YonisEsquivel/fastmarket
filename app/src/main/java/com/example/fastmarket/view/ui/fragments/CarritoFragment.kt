00d84c3c10acb4d8dc4b05274f7ac0b695ef9bd0
Yonis Esquivel <info.contactoprofesional@gmail.com>
2022-11-18 17:08:49 -0500

    Eliminar producto

--- app/src/main/java/com/example/fastmarket/view/ui/fragments/CarritoFragment.kt
+++ app/src/main/java/com/example/fastmarket/view/ui/fragments/CarritoFragment.kt
@@ -1,16 +1,19 @@
 package com.example.fastmarket.view.ui.fragments
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.airbnb.lottie.parser.IntegerParser
 import com.example.fastmarket.R
 import com.example.fastmarket.model.compras
 import com.example.fastmarket.view.adapter.ComprasAdapter
@@ -19,15 +22,19 @@ import com.example.fastmarket.viewmodel.ComprasViewModel
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 
 @Suppress("DEPRECATION")
 class CarritoFragment : Fragment(), OnCompraItemClickListener {
 
     lateinit var firebaseAuth: FirebaseAuth
+    val database:FirebaseFirestore=FirebaseFirestore.getInstance()
     lateinit var recyclerView: RecyclerView
     lateinit var adapter: ComprasAdapter
     private val viewmodel by lazy {ViewModelProvider(this).get(ComprasViewModel::class.java)}
+    lateinit var precioT:TextView
+    lateinit var bottoncompra:TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,11 +46,17 @@ class CarritoFragment : Fragment(), OnCompraItemClickListener {
         savedInstanceState: Bundle?
     ): View? {
         val view=inflater.inflate(R.layout.fragment_carrito, container, false)
+        precioT=view.findViewById(R.id.precio_total)
+        bottoncompra=view.findViewById(R.id.boton_compra)
         recyclerView=view.findViewById(R.id.recyclerviewcompra)
         adapter= ComprasAdapter(requireContext(), this)
         recyclerView.layoutManager=LinearLayoutManager(context)
         recyclerView.adapter=adapter
         observedata()
+        preciototal()
+        bottoncompra.setOnClickListener {
+            realizarcompra()
+        }
         return view
     }
 
@@ -78,7 +91,35 @@ class CarritoFragment : Fragment(), OnCompraItemClickListener {
     }
 
     override fun OnItemClick(mercado: compras, position: Int) {
-        TODO("Not yet implemented")
+        database.collection("compras")
+            .document(mercado.producto)
+            .delete()
+    }
+
+    private fun preciototal(){
+        database.collection("compras")
+            .get()
+            .addOnSuccessListener { result->
+                val preciouni= mutableListOf<String>()
+                for (document in result){
+                    val precio=document["precio"].toString()
+                    preciouni.add(precio!!)
+                }
+                val preciototal =preciouni.mapNotNull { it.toIntOrNull() }.sum()
+                precioT.setText(Integer.toString(preciototal))
+            }
+    }
+
+    private fun realizarcompra(){
+        val builder=AlertDialog.Builder(requireContext())
+        builder.setTitle("FastMarket")
+        builder.setMessage("¿Desea finalizar su compra?")
+        builder.setPositiveButton("Aceptar"){
+            dialog,which->
+            findNavController().navigate(R.id.action_carritoFragment_to_homeFragment)
+        }
+        builder.setNegativeButton("Cancelar", null)
+        builder.show()
     }
 
 }
